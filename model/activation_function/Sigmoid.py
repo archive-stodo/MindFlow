98d997acc001887d45e1aaa88216e9168f35a906
dawid <stodoo@gmail.com>
2019-10-26 19:00:15 +0200

    Sigmoid derivative added

--- model/activation_function/Sigmoid.py
+++ model/activation_function/Sigmoid.py
@@ -5,6 +5,9 @@ import numpy as np
 class Sigmoid(ActivationFunction):
 
     @classmethod
-    def get_value(cls, input_x):
+    def value(cls, input_x):
         return 1 / (1 + np.exp(-input_x))
 
+    @classmethod
+    def derivative(cls, input_x):
+        return cls.value(input_x) * (1 - cls.value(input_x))
--- test/model/activation_function/TestSigmoid.py
+++ test/model/activation_function/TestSigmoid.py
@@ -6,14 +6,28 @@ class TestSigmoid(unittest.TestCase):
 
     def test_sigmoid_at_zero(self):
         # when
-        sigmoid_at_zero = Sigmoid.get_value(0)
+        sigmoid_at_zero = Sigmoid.value(0)
 
         # then
         self.assertEqual(sigmoid_at_zero, 0.5)
 
-    def test_sigmoid_at_one_ten(self):
+    def test_sigmoid_derivative_at_zero(self):
         # when
-        sigmoid_at_ten = Sigmoid.get_value(10)
+        sigmoid_derivative_at_zero = Sigmoid.derivative(0)
+
+        # then
+        self.assertEqual(sigmoid_derivative_at_zero, 0.25)
+
+    def test_sigmoid_at_ten(self):
+        # when
+        sigmoid_at_ten = Sigmoid.value(10)
 
         # then
         self.assertAlmostEqual(sigmoid_at_ten, 0.9999546)
+
+    def test_sigmoid_derivative_at_ten(self):
+        # when
+        sigmoid_derivative_at_ten = Sigmoid.derivative(10)
+
+        # then
+        self.assertAlmostEqual(sigmoid_derivative_at_ten, 0.00004539)
